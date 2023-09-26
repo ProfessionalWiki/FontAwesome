2a748c889f9eadffc85c1855e22e7cb1f0295331
Wouter Rademaker <wouter.rademaker@egel.org>
2023-09-26 11:15:54 +0200

    Update WebfontRenderer.php
    Add Style

--- src/IconRenderers/WebfontRenderer.php
+++ src/IconRenderers/WebfontRenderer.php
@@ -63,7 +63,11 @@ class WebfontRenderer implements IconRenderer {
 
 		$this->registerRlModule( $parser );
 
-		return Html::element( 'i', [ 'class' => [ $this->fontClass, 'fa-' . trim( $frame->expand( $args[ 0 ] ) ) ] ] );
+		switch (sizeof($args)) {
+			case "1": return Html::element( 'i', [ 	'class' => [ $this->fontClass, 'fa-' . trim( $frame->expand( $args[ 0 ] ) ) ] ] );
+		  	default : return Html::element( 'i', [ 	'class' => [ $this->fontClass, 'fa-' . trim( $frame->expand( $args[ 0 ] ) ) ] ,
+								'style' => trim( $frame->expand( $args[ 1 ] ) )                               ] );
+		}
 	}
 
 	/**
